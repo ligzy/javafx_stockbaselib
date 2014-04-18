e3937076a2ed457972d7a07e9e019c572f476d87
unknown <doyin@DOYIN-CN.cn.oracle.com>
2014-04-18 13:37:29 +0800

    2

---
+++ src/common/answer/bean/dto/Liangrongtarget.java
@@ -0,0 +1,21 @@
+package common.answer.bean.dto;
+
+
+
+public class Liangrongtarget {
+    private String stock_cd;
+   
+
+    public Liangrongtarget() { }
+
+  
+    public String getStock_cd() {
+        return stock_cd;
+    }
+
+    public void setStock_cd(String stock_cd) {
+        this.stock_cd = stock_cd;
+    }
+
+   
+}
---
+++ src/common/answer/bean/dto/Ztgs.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package common.answer.bean.dto;
+
+/**
+ *
+ * @author doyin
+ */
+public class Ztgs {
+    
+     private String record_date;
+     private Integer ztgs;
+
+    public String getRecord_date() {
+        return record_date;
+    }
+
+    public void setRecord_date(String record_date) {
+        this.record_date = record_date;
+    }
+
+    public Integer getZtgs() {
+        return ztgs;
+    }
+
+    public void setZtgs(Integer ztgs) {
+        this.ztgs = ztgs;
+    }
+     
+    
+}
